80dd2f3ddda679f05ea74368577e9b08773f0cbe
OwenPFC <owenpfcavanaugh@gmail.com>
2026-01-31 15:34:57 -0500

    Input good, core mechanics done

--- scripts/byte.gd
+++ scripts/byte.gd
@@ -4,7 +4,7 @@ extends PathFollow2D
 signal reached_end
 
 
-@export var speed: float = 0.1
+@export var speed: float = 0.01
 
 
 var number: String = "00000000"
@@ -12,11 +12,8 @@ var number: String = "00000000"
 
 func _ready() -> void:
 	var num = randi_range(0, 255)
-	print(num)
 	number = int_to_binary(num)
 	$number.text = "[b]" + number
-	print($number.text)
-
 
 func _process(delta: float):
 	var new_progress_ratio = progress_ratio + (delta * speed)
@@ -27,6 +24,10 @@ func _process(delta: float):
 		queue_free()
 
 	progress_ratio = new_progress_ratio
+	
+func set_number(n:String):
+	number = n
+	$number.text = "[b]" + number
 	
 func int_to_binary(n:int):
 	var bin = String.num_uint64(n,2)
