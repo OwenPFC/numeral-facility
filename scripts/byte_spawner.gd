c56fc2b5cb573f0a9d6ab0c805e7a4091bbb47ae
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2026-01-30 21:56:37 -0500

    byte spawner & conveyor

---
+++ scripts/byte_spawner.gd
@@ -0,0 +1,16 @@
+class_name ByteSpawner
+extends Node2D
+
+
+@export var byte_scene: PackedScene
+
+
+func _on_spawn_timer_timeout():
+	# the conveyor must exist
+	if Conveyor.instance == null:
+		return
+
+	var byte: Node2D = byte_scene.instantiate()
+	byte.global_position = global_position
+	Conveyor.instance.conveyor_path.add_child(byte)
+
