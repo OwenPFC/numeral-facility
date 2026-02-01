1dde15257900dbf85f6ebe2c3cffe6b3d6feabbb
OwenPFC <owenpfcavanaugh@gmail.com>
2026-02-01 10:52:45 -0500

    gameplay somewhat fixed

--- scripts/numbers.gd
+++ scripts/numbers.gd
@@ -21,16 +21,16 @@ func band(x:String,y:String):
 	var n = x.bin_to_int() & y.bin_to_int()
 	return int_to_binary(n)
 	
-#func nand(x:String, y:String):
-	#var n = bnot(x.bin_to_int() & y.bin_to_int())
-	#return int_to_binary(n)
+func nand(x:String, y:String):
+	var n = bnot(x.bin_to_int() & y.bin_to_int())
+	return int_to_binary(n)
 	
-#func bnot(x:int):
-	#var MAX_INT: int = 2**63 - 1
-	#MAX_INT <<= 8;
-	#var padded = MAX_INT | x
-	#padded = ~padded
-	#return padded
+func bnot(x:int):
+	var MAX_INT: int = 2**63 - 1
+	MAX_INT <<= 8;
+	var padded = MAX_INT | x
+	padded = ~padded
+	return padded
 	
 func int_to_binary(n:int):
 	var bin = String.num_uint64(n,2)
